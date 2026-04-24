26d9d1bc56120e4ab5fa88ff26daec03972e4fa3
11235798 <al.salgador@duocuc.cl>
2026-04-24 12:13:15 -0400

    Add files via upload

---
+++ ms-descuentos/src/test/java/cl/triskeledu/descuentos/TiendaDescuentosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.triskeledu.descuentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaDescuentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
