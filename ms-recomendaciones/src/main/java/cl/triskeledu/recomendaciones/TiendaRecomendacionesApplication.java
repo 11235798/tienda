45c05eb42f781da15471e5f39ecd0487a3136160
11235798 <al.salgador@duocuc.cl>
2026-04-27 11:20:15 -0400

    Add files via upload

---
+++ ms-recomendaciones/src/main/java/cl/triskeledu/recomendaciones/TiendaRecomendacionesApplication.java
@@ -0,0 +1,13 @@
+package cl.triskeledu.recomendaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaRecomendacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaRecomendacionesApplication.class, args);
+	}
+
+}
