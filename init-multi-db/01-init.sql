8bf90287f242d8a141b282de3a33cde9462798b6
11235798 <al.salgador@duocuc.cl>
2026-05-15 01:42:44 -0400

    Update 01-init.sql

--- init-multi-db/01-init.sql
+++ init-multi-db/01-init.sql
@@ -1,39 +1,26 @@
 -- Usuarios
-SELECT 'CREATE DATABASE usuarios'
-WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'usuarios') \gexec
+SELECT 'CREATE DATABASE db_usuarios' WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'db_usuarios') \gexec
 
--- Catálogo de juegos
-SELECT 'CREATE DATABASE catalogo'
-WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'catalogo') \gexec
+-- Catalogo de juegos
+SELECT 'CREATE DATABASE db_catalogo' WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'db_catalogo') \gexec
 
 -- Inventario
-SELECT 'CREATE DATABASE inventario'
-WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'inventario') \gexec
+SELECT 'CREATE DATABASE db_inventario' WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'db_inventario') \gexec
 
 -- Carrito de compras
-SELECT 'CREATE DATABASE compras'
-WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'compras') \gexec
+SELECT 'CREATE DATABASE db_compras' WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'db_compras') \gexec
 
 -- Pagos
-SELECT 'CREATE DATABASE pagos'
-WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'pagos') \gexec
+SELECT 'CREATE DATABASE db_pagos' WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'db_pagos') \gexec
 
 -- Pedidos
-SELECT 'CREATE DATABASE pedidos'
-WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'pedidos') \gexec
+SELECT 'CREATE DATABASE db_pedidos' WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'db_pedidos') \gexec
 
 -- Recomendaciones
-SELECT 'CREATE DATABASE recomendaciones'
-WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'recomendaciones') \gexec
+SELECT 'CREATE DATABASE db_recomendaciones' WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'db_recomendaciones') \gexec
 
 -- Reseñas
-SELECT 'CREATE DATABASE resenas'
-WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'resenas') \gexec
+SELECT 'CREATE DATABASE db_resenas' WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'db_resenas') \gexec
 
 -- Notificaciones
-SELECT 'CREATE DATABASE notificaciones'
-WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'notificaciones') \gexec
-
--- Reseñas
-SELECT 'CREATE DATABASE resenas'
-WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'resenas') \gexec
+SELECT 'CREATE DATABASE db_notificaciones' WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'db_notificaciones') \gexec
