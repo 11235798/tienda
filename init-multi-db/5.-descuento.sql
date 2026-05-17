f25bef33f5f56f3bdb1b90dfac1622c0a93c3b7c
Arcos-Barjas <dam.arcos@duocuc.cl>
2026-05-17 19:13:03 -0400

    Update 5.-descuento.sql
    Ordené, añadí un "INSERT INTO cupones_uso", y cambié "precio_base" de DECIMAL a INT para pasar de dólares a pesos chilenos

--- init-multi-db/5.-descuento.sql
+++ init-multi-db/5.-descuento.sql
@@ -5,21 +5,22 @@ DROP TABLE IF EXISTS cupones;
 DROP TABLE IF EXISTS juegos_proyeccion;
 
 CREATE TABLE juegos_proyeccion (
-    sku VARCHAR(50) PRIMARY KEY,
-    precio_base DECIMAL(10,2)
+    sku         VARCHAR(50)     UNIQUE  PRIMARY KEY,
+    precio_base INT
 );
 
 CREATE TABLE cupones (
-    codigo VARCHAR(20) PRIMARY KEY,
-    porcentaje_dcto INTEGER CHECK (porcentaje_dcto <= 100),
-    activo BOOLEAN DEFAULT TRUE
+    codigo          VARCHAR(20) UNIQUE  PRIMARY KEY,
+    porcentaje_dcto INTEGER     CHECK (porcentaje_dcto <= 100),
+    activo          BOOLEAN     DEFAULT TRUE
 );
 
 CREATE TABLE cupones_uso (
-    id SERIAL PRIMARY KEY,
-    cupon_codigo VARCHAR(20) REFERENCES cupones(codigo),
-    aplicado_a_sku VARCHAR(50) REFERENCES juegos_proyeccion(sku)
+    id              SERIAL      PRIMARY KEY,
+    cupon_codigo    VARCHAR(20) REFERENCES cupones(codigo),
+    aplicado_a_sku  VARCHAR(50) REFERENCES juegos_proyeccion(sku)
 );
 
-INSERT INTO juegos_proyeccion VALUES ('ELDER-001', 59.99);
+INSERT INTO juegos_proyeccion VALUES ('ELDER-001', 59000);
 INSERT INTO cupones VALUES ('OFFER20', 20, TRUE);
+INSERT INTO cupones_uso (cupon_codigo, aplicado_a_sku) VALUES ('OFFER20', 'ELDER-001')
