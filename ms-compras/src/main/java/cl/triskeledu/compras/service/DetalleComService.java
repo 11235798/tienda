13330260ed1940fb80a20683861f1ca4f8f87b77
Arcos-Barjas <dam.arcos@duocuc.cl>
2026-06-08 11:55:44 -0400

    Añadí ms-compras -> service
    (Incompletos)

---
+++ ms-compras/src/main/java/cl/triskeledu/compras/service/DetalleComService.java
@@ -0,0 +1,32 @@
+package cl.triskeledu.compras.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+// import org.springframework.stereotype.Service;
+
+// import cl.triskeledu.catalogo.client.RecursoClient;
+// import cl.triskeledu.catalogo.dto.LibroRequest;
+// import cl.triskeledu.catalogo.dto.LibroResponse;
+// import cl.triskeledu.catalogo.event.LibroEventProducer;
+// import cl.triskeledu.common.event.LibroCreatedEvent;
+// import cl.triskeledu.common.event.LibroDeletedEvent;
+// import cl.triskeledu.common.event.LibroUpdatedEvent;
+// import cl.triskeledu.common.exception.*;
+// import cl.triskeledu.catalogo.mapper.LibroMapper;
+// import cl.triskeledu.catalogo.model.Categoria;
+// import cl.triskeledu.catalogo.model.Libro;
+// import cl.triskeledu.catalogo.repository.CategoriaRepository;
+// import cl.triskeledu.catalogo.repository.LibroRepository;
+// import jakarta.transaction.Transactional;
+// import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class DetalleComService {
+
+}
