695bf8111fcc9d4159f7bf19ed1d28bf46fc9bb3
lostephens-code <lo.stephens@duocuc.cl>
2026-06-15 15:15:38 -0400

    Resena v2 done

--- ms-resenas/src/main/java/cl/triskeledu/resenas/service/ResenaService.java
+++ ms-resenas/src/main/java/cl/triskeledu/resenas/service/ResenaService.java
@@ -5,10 +5,14 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import cl.triskeledu.resenas.dto.ResenaRequest;
+import cl.triskeledu.resenas.dto.ResenaResponse;
+import cl.triskeledu.resenas.mapper.ResenaMapper;
 import cl.triskeledu.resenas.model.Resena;
 import cl.triskeledu.resenas.model.Usuario;
 import cl.triskeledu.resenas.model.Videojuego;
 import cl.triskeledu.resenas.repository.ResenasRepository;
+import cl.triskeledu.resenas.repository.UsuarioRepository;
+import cl.triskeledu.resenas.repository.VideojuegoRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -16,25 +20,39 @@ import lombok.RequiredArgsConstructor;
 public class ResenaService {
 
     private final ResenasRepository resenasRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final VideojuegoRepository videojuegoRepository;
+    private final ResenaMapper resenaMapper;
 
-    public List<Resena> obtenerTodas() {
-        return resenasRepository.findAll();
+    public List<ResenaResponse> obtenerTodas() {
+        return resenasRepository.findAll()
+                .stream()
+                .map(resenaMapper::toResponse)
+                .toList();
     }
 
-    public Resena obtenerPorId(Integer id) {
-        return resenasRepository.findById(id)
+    public ResenaResponse obtenerPorId(Integer id) {
+        Resena resena = resenasRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Reseña no encontrada"));
+
+        return resenaMapper.toResponse(resena);
     }
 
-    public List<Resena> obtenerPorUsuario(Integer usuarioId) {
-        return resenasRepository.findByUsuarioId(usuarioId);
+    public List<ResenaResponse> obtenerPorUsuario(Integer usuarioId) {
+        return resenasRepository.findByUsuarioId(usuarioId)
+                .stream()
+                .map(resenaMapper::toResponse)
+                .toList();
     }
 
-    public List<Resena> obtenerPorVideojuego(Integer videojuegoId) {
-        return resenasRepository.findByVideojuegoId(videojuegoId);
+    public List<ResenaResponse> obtenerPorVideojuego(Integer videojuegoId) {
+        return resenasRepository.findByVideojuegoId(videojuegoId)
+                .stream()
+                .map(resenaMapper::toResponse)
+                .toList();
     }
 
-    public Resena guardar(ResenaRequest request) {
+    public ResenaResponse guardar(ResenaRequest request) {
 
         Usuario usuario = usuarioRepository.findById(request.getUsuarioId())
                 .orElseThrow(() -> new RuntimeException("Usuario no existe"));
@@ -42,7 +60,7 @@ public class ResenaService {
         Videojuego videojuego = videojuegoRepository.findById(request.getVideojuegoId())
                 .orElseThrow(() -> new RuntimeException("Videojuego no existe"));
 
-        return resenasRepository
+        Resena resenaGuardada = resenasRepository
                 .findByUsuarioIdAndVideojuegoId(
                         request.getUsuarioId(),
                         request.getVideojuegoId())
@@ -65,6 +83,8 @@ public class ResenaService {
 
                     return resenasRepository.save(nueva);
                 });
+
+        return resenaMapper.toResponse(resenaGuardada);
     }
 
     public void eliminar(Integer id) {
