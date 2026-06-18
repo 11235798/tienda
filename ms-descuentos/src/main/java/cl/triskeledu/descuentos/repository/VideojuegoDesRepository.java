314001f703fff2bee2f813d3763ee4a05697ed8d
Arcos-Barjas <dam.arcos@duocuc.cl>
2026-06-17 21:45:21 -0400

    Update VideojuegoDesRepository.java

--- ms-descuentos/src/main/java/cl/triskeledu/descuentos/repository/VideojuegoDesRepository.java
+++ ms-descuentos/src/main/java/cl/triskeledu/descuentos/repository/VideojuegoDesRepository.java
@@ -1,5 +1,8 @@
 package cl.triskeledu.descuentos.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
@@ -8,6 +11,13 @@ import cl.triskeledu.descuentos.model.VideojuegoDescuento;
 
 @Repository
 public interface VideojuegoDesRepository extends JpaRepository<VideojuegoDescuento, Long> {
+    Optional<VideojuegoDescuento> findByVideojuegoId(Long vidId);
+    
+    Optional<VideojuegoDescuento> findByCampanaId(Long camId);
+    
     boolean existsByVideojuegoId(Long id);
+
     boolean existsByCampanaId(Long id);
+
+    List<VideojuegoDescuento> findByEstado(String estado);
 }
