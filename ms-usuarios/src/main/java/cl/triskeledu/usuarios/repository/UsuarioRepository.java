8560e8b7fbf21624fd56dff63a71672a21ef4ae8
lostephens-code <lo.stephens@duocuc.cl>
2026-07-01 09:08:09 -0400

    otro error corregido

--- ms-usuarios/src/main/java/cl/triskeledu/usuarios/repository/UsuarioRepository.java
+++ ms-usuarios/src/main/java/cl/triskeledu/usuarios/repository/UsuarioRepository.java
@@ -3,11 +3,9 @@ package cl.triskeledu.usuarios.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import cl.triskeledu.usuarios.model.Usuario;
 
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     Optional<Usuario> findByEmail(String email);
