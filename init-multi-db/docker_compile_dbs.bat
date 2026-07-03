429bddf664e637605e8c1a0510a44bdca7de0bf8
Arcos-Barjas <dam.arcos@duocuc.cl>
2026-07-03 10:52:36 -0400

    Update docker_compile_dbs.bat

--- init-multi-db/docker_compile_dbs.bat
+++ init-multi-db/docker_compile_dbs.bat
@@ -1,8 +1,6 @@
-docker exec -i postgres_rdbms psql -U postgres -d main_postgres_db < 01-init.sql
-docker exec -i postgres_rdbms psql -U postgres -d main_postgres_db < 2.-usuarios.sql
-docker exec -i postgres_rdbms psql -U postgres -d main_postgres_db < 3.-catalogo.sql
-docker exec -i postgres_rdbms psql -U postgres -d main_postgres_db < 5.-descuento.sql
-docker exec -i postgres_rdbms psql -U postgres -d main_postgres_db < 7.-compras.sql
-docker exec -i postgres_rdbms psql -U postgres -d main_postgres_db < 9.resenas.sql
-
-
+docker exec postgres-rdbms psql -U postgres -d main_postgres_db -f /docker-entrypoint-initdb.d/01-init.sql
+docker exec postgres-rdbms psql -U postgres -d main_postgres_db -f /docker-entrypoint-initdb.d/2.-usuarios.sql
+docker exec postgres-rdbms psql -U postgres -d main_postgres_db -f /docker-entrypoint-initdb.d/3.-catalogo.sql
+docker exec postgres-rdbms psql -U postgres -d main_postgres_db -f /docker-entrypoint-initdb.d/5.-descuento.sql
+docker exec postgres-rdbms psql -U postgres -d main_postgres_db -f /docker-entrypoint-initdb.d/7.-compras.sql
+docker exec postgres-rdbms psql -U postgres -d main_postgres_db -f /docker-entrypoint-initdb.d/9.resenas.sql
