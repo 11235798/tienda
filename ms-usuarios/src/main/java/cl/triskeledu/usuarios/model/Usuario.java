06f87da2c85677ffa253521f83688264776e1790
lostephens-code <lo.stephens@duocuc.cl>
2026-07-03 12:13:29 -0400

    Usuarios arreglado

--- ms-usuarios/src/main/java/cl/triskeledu/usuarios/model/Usuario.java
+++ ms-usuarios/src/main/java/cl/triskeledu/usuarios/model/Usuario.java
@@ -29,7 +29,6 @@ public class Usuario {
     @Column(name = "password", nullable = false, length = 255)
     private String password;
 
-    @Enumerated(EnumType.STRING)
     @Column(name = "rol", nullable = false, length = 50)
     private String rol;
 
